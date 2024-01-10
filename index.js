52041f976fe94b6291c0bb296702ea62b89ef776
Mohammad Gufran Chouhan <149248964+Gufran369@users.noreply.github.com>
2024-01-10 21:44:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
+
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if (randomImageSource1 > randomImageSource2){
+    document.querySelector("h1").innerHTML="Player 1 is winner";
+}else if (randomImageSource2 > randomImageSource1){
+    document.querySelector("h1").innerHTML="Player 2 is winner";
+}else{
+    document.querySelector("h1").innerHTML = "Draw"
+}
